4243527d9cc6abc8432b57eb02221145acbe0ff2
vagrant <ttjho@hotmail.com>
2020-11-09 19:28:12 +0000

    initial commit, add player class

---
+++ player_class.rb
@@ -0,0 +1,17 @@
+class Player
+  attr_accessor :name, :life
+  
+  def initialize(name)
+    @name = name
+    @life = 3
+  end
+  
+  def life_loss()
+    @life -= 1
+  end
+
+end
+
+jay = Player.new("Jay")
+jay.life_loss
+puts jay.life
